afonsocastro96/universal-pokemon-randomizer-zx

Classify trainers as potential or guaranteed multi-battles

src/com/dabomstew/pkrandom/constants/Gen6Constants.java
@@ -1076,28 +1076,28 @@ public class Gen6Constants {
         }
     }
 
-    public static void setCouldBeMultiBattleXY(List<Trainer> trs) {
+    public static void setMultiBattleStatusXY(List<Trainer> trs) {
         // 108 + 111: Team Flare Grunts in Glittering Cave
         // 348 + 350: Team Flare Celosia and Bryony fight in Poké Ball Factory
         // 438 + 439: Tierno and Trevor fight on Route 7
         // 470 + 611, 472 + 610, 476 + 612: Team Flare Admin and Grunt fights in Team Flare Secret HQ
-        setCouldBeMultiBattle(trs, 108, 111, 348, 350, 438, 439, 470, 472, 476, 610, 611, 612);
+        setAlwaysMultiBattleStatus(trs, 108, 111, 348, 350, 438, 439, 470, 472, 476, 610, 611, 612);
     }
 
-    public static void setCouldBeMultiBattleORAS(List<Trainer> trs) {
+    public static void setMultiBattleStatusORAS(List<Trainer> trs) {
         // 683 + 904: Aqua Admin Matt and Team Aqua Grunt fight on the Southern Island
         // 687 + 905: Aqua Admin Matt and Team Aqua Grunt fight at the Mossdeep Space Center
         // 688 + 903: Aqua Admin Shelly and Team Aqua Grunt fight in Meteor Falls
         // 691 + 902: Magma Admin Tabitha and Team Magma Grunt fight in Meteor Falls
         // 694 + 900: Magma Admin Courtney and Team Magma Grunt fight on the Southern Island
         // 698 + 901: Magma Admin Courtney and Team Magma Grunt fight at the Mossdeep Space Center
-        setCouldBeMultiBattle(trs, 683, 687, 688, 691, 694, 698, 900, 901, 902, 903, 904, 905);
+        setAlwaysMultiBattleStatus(trs, 683, 687, 688, 691, 694, 698, 900, 901, 902, 903, 904, 905);
     }
 
-    private static void setCouldBeMultiBattle(List<Trainer> allTrainers, int... numbers) {
+    private static void setAlwaysMultiBattleStatus(List<Trainer> allTrainers, int... numbers) {
         for (int num : numbers) {
             if (allTrainers.size() > (num - 1)) {
-                allTrainers.get(num - 1).couldBeMultiBattle = true;
+                allTrainers.get(num - 1).multiBattleStatus = Trainer.MultiBattleStatus.ALWAYS;
             }
         }
     }

alienth/ironmon-randomizer

src/com/dabomstew/pkrandom/constants/Gen6Constants.java
@@ -1076,28 +1076,28 @@ public class Gen6Constants {
         }
     }
 
-    public static void setCouldBeMultiBattleXY(List<Trainer> trs) {
+    public static void setMultiBattleStatusXY(List<Trainer> trs) {
         // 108 + 111: Team Flare Grunts in Glittering Cave
         // 348 + 350: Team Flare Celosia and Bryony fight in Poké Ball Factory
         // 438 + 439: Tierno and Trevor fight on Route 7
         // 470 + 611, 472 + 610, 476 + 612: Team Flare Admin and Grunt fights in Team Flare Secret HQ
-        setCouldBeMultiBattle(trs, 108, 111, 348, 350, 438, 439, 470, 472, 476, 610, 611, 612);
+        setMultiBattleStatus(trs, 108, 111, 348, 350, 438, 439, 470, 472, 476, 610, 611, 612);
     }
 
-    public static void setCouldBeMultiBattleORAS(List<Trainer> trs) {
+    public static void setMultiBattleStatusORAS(List<Trainer> trs) {
         // 683 + 904: Aqua Admin Matt and Team Aqua Grunt fight on the Southern Island
         // 687 + 905: Aqua Admin Matt and Team Aqua Grunt fight at the Mossdeep Space Center
         // 688 + 903: Aqua Admin Shelly and Team Aqua Grunt fight in Meteor Falls
         // 691 + 902: Magma Admin Tabitha and Team Magma Grunt fight in Meteor Falls
         // 694 + 900: Magma Admin Courtney and Team Magma Grunt fight on the Southern Island
         // 698 + 901: Magma Admin Courtney and Team Magma Grunt fight at the Mossdeep Space Center
-        setCouldBeMultiBattle(trs, 683, 687, 688, 691, 694, 698, 900, 901, 902, 903, 904, 905);
+        setMultiBattleStatus(trs, 683, 687, 688, 691, 694, 698, 900, 901, 902, 903, 904, 905);
     }
 
-    private static void setCouldBeMultiBattle(List<Trainer> allTrainers, int... numbers) {
+    private static void setMultiBattleStatus(List<Trainer> allTrainers, int... numbers) {
         for (int num : numbers) {
             if (allTrainers.size() > (num - 1)) {
-                allTrainers.get(num - 1).couldBeMultiBattle = true;
+                allTrainers.get(num - 1).multiBattleStatus = Trainer.MultiBattleStatus.ALWAYS;
             }
         }
     }